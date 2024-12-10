7f617adf136291eb74ece6e58edb44469938a98d
Willy Douhard <willy.douhard@gmail.com>
2024-12-10 17:28:10 +0100

    feat: load .env

--- backend/looplit/__init__.py
+++ backend/looplit/__init__.py
@@ -1,3 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+env_found = load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
+
+from looplit.logger import logger
+
+if env_found:
+    logger.info("Loaded .env file")
+
 from looplit.decorators import stateful, tool
 from looplit.state import State
 
